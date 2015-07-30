b1773bf2c25b94dc52a52b27031dab5dbc78da66
Ben Fairless <ben.fairless@landregistry.gsi.gov.uk>
2015-07-30 10:08:47 +0100

    Added relationships between python requirement installation and repository pulls

--- manifests/application.pp
+++ manifests/application.pp
@@ -150,10 +150,11 @@ define wsgi::application (
     }
 
     exec { "${name} dependencies":
-      command => "${venv_dir}/bin/pip install -r ${code_dir}/requirements.txt",
-      user    => $owner,
-      group   => $group,
-      require => [Exec["${name} virtualenv"], File["${name} requirements.txt"]],
+      command   => "${venv_dir}/bin/pip install -r ${code_dir}/requirements.txt",
+      user      => $owner,
+      group     => $group,
+      require   => [Exec["${name} virtualenv"], File["${name} requirements.txt"]],
+      subscribe => Vcsrepo[$code_dir]
       #refreshonly => true
     }
 
