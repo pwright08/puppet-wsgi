92c7e155fd128d1bf67e424ae8169189f3853c6b
Matt Gaspar <matt.gaspar@digital.landregistry.gov.uk>
2017-05-04 12:23:39 +0100

    Implement FlywayDB for Java applications

--- manifests/dependencies/java.pp
+++ manifests/dependencies/java.pp
@@ -14,4 +14,28 @@ class wsgi::dependencies::java () {
   # that Java is installed. We do not need explicit 'ownership' of this resource
   ensure_packages($packages, {'ensure' => 'present'})
 
+  # Install FlywayDB Command Line Interface to manage Postgresql databases
+  # that are owned by Java applications
+  $flyway_db_version = '4.1.2'
+  archive {"flyway_db_${flyway_db_version}":
+    ensure     => present,
+    url        => "https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/${flyway_db_version}/flyway-commandline-${flyway_db_version}.tar.gz",
+    target     => '/opt',
+    root_dir   => '.',
+    checksum   => false,
+    src_target => '/tmp'
+  }
+
+  file {"/opt/flyway-${flyway_db_version}/flyway":
+    ensure  => present,
+    mode    => '0755',
+    require => Archive["flyway_db_${flyway_db_version}"]
+  }
+
+  file {'/usr/bin/flyway':
+    ensure  => 'link',
+    target  => "/opt/flyway-${flyway_db_version}/flyway",
+    require => File["/opt/flyway-${flyway_db_version}/flyway"]
+  }
+
 }
